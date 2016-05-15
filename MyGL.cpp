01967791be3e7c825935ded9cc3e4fca67ebcf0c
Richie Kirchofer <rgkirch@gmail.com>
2016-05-15 16:11:31 -0400

    code works. trying to get vm to work

--- MyGL.cpp
+++ MyGL.cpp
@@ -587,7 +587,7 @@ GLFWwindow* MyGL::makeWindowForContext() {
     int height = 1;
 
 	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-	//glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
 	//glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 	glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     //glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE ); /** for mac compatability*/
