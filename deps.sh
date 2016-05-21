ed50ef3348728cfc448e8f70ec41cb4a428a879c
Richie Kirchofer <rgkirch@gmail.com>
2016-05-21 11:00:59 -0400

    readme and deps

--- deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo apt-get install libglew-dev libglfw3-dev libglm-dev libboost-dev
-MODULES=$(find /usr/share/*cmake* -name 'FindGLEW.cmake' -printf '%h\n')
-sudo cp -v ./cmakeModules/FindGLFW.cmake $MODULES
-
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-sudo apt-get install libglew-dev libglfw3-dev libglm-dev
+sudo apt-get install libglew-dev libglfw3-dev libglm-dev libboost-dev libboost-system-dev libboost-filesystem-dev libmagick++-dev
+MODULES=$(find /usr/share/*cmake* -name 'FindGLEW.cmake' -printf '%h\n')
+sudo cp -v ./cmakeModules/FindGLFW.cmake $MODULES
 
-
+# note, ubuntu 14.04 and before does not have libglfw3-dev
+# on account of that, this will fail. remove libglfw3-dev and try with libglfw-dev
