33c48566560f42907996ac076d11ff072496fd1f
richie <rgkirch@gmail.com>
2016-06-22 13:37:41 -0500

    test works with join outside, time for custom delete

--- test/unique-ptr-to-thread-custom-delete.cpp
+++ test/unique-ptr-to-thread-custom-delete.cpp
@@ -18,8 +18,7 @@ void function(std::list<int>& list, int argument)
 int main(int argc, char* argv[])
 {
     std::list<int> list;
-    std::unique_ptr<std::thread> thread = std::make_unique<std::thread>(std::bind(function, list, 7));
-    //std::unique_ptr<std::thread> thread = std::make_unique<std::thread>([&] {function(list, 7);});
+    std::unique_ptr<std::thread> thread = std::make_unique<std::thread>(std::bind(function, std::ref(list), 7));
     thread.get()->join();
     for(auto& x : list)
     {
