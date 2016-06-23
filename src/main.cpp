f207b6134d52168973cdf95aadf6192f4621e67d
richie <rgkirch@gmail.com>
2016-06-23 12:27:25 -0500

    code works, took program options out of main becasue...

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "MyGL.hpp"
 
 int main(int argc, char* argv[]) {
-    boost::program_options::option_description desc;
+    //boost::program_options::option_description desc;
     std::unique_ptr<MyGL> mygl;
     if(argc > 1) {
         mygl = std::make_unique<MyGL>(std::string(argv[1]));
