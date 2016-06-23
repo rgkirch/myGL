327db0aa8f4431aeed7a9b1bb8bcdd3b0a957120
richie <rgkirch@gmail.com>
2016-06-23 10:29:10 -0500

    self joining threads test code works great

--- test/unique-ptr-to-thread-custom-delete.cpp
+++ test/unique-ptr-to-thread-custom-delete.cpp
@@ -10,16 +10,26 @@ void function(std::list<int>& list, int argument)
     assert(argument > 0);
     while(argument)
     {
+        std::cout << "loop" << std::endl;
         list.push_back(argument);
         --argument;
     }
 }
 
+void joiner(std::thread* thread)
+{
+    std::cout << "join start" << std::endl;
+    thread->join();
+    std::cout << "join end" << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
     std::list<int> list;
-    std::unique_ptr<std::thread> thread = std::make_unique<std::thread>(std::bind(function, std::ref(list), 7));
-    thread.get()->join();
+    //std::unique_ptr<std::thread> thread = std::make_unique<std::thread>(std::bind(function, std::ref(list), 7));
+    //std::unique_ptr<std::thread> thread(new std::thread(std::bind(function, std::ref(list), 7)));
+    std::unique_ptr<std::thread, void (*)(std::thread*)> thread(new std::thread(std::bind(function, std::ref(list), 7)), joiner);
+    //thread.get()->join();
     for(auto& x : list)
     {
         std::cout << x << std::endl;
